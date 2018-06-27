587b457bb85066cfdd55f7f87b4a6cb3febfbba6
Diukarev <diukarev.sergii@gmail.com>
2018-06-27 11:49:34 +0300

    added module4-solution

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
 
     var yeomanConfig = {
         // app: 'app',
-        app: 'module3-solution',
+        app: 'module4-solution',
         // app: 'form',
         dist: 'dist'
     };
